29e51ff558783f5b1fdb771f1a54684b175b8473
TSaiCharan <sai60095@gmail.com>
2021-04-15 14:42:00 +0530

    .

--- AWT_Lab/12-04-2021/weather/weather.js
+++ AWT_Lab/12-04-2021/weather/weather.js
@@ -4,7 +4,7 @@ function fetchWeather(){
     var request=new XMLHttpRequest();
     var apiKey='2578a5dfba20e6f114d57a2840d9561f';
     var city=document.getElementById("city").value;
-    var url=`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
     // console.log("running");
     request.open('GET',url,true);
     request.onload=function()
