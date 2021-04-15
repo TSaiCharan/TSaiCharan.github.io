8cd839786f8632ef4411516cfaa066c7190d622c
TSaiCharan <sai60095@gmail.com>
2021-04-15 14:54:05 +0530

    .

--- AWT_Lab/12-04-2021/weather/weather.js
+++ AWT_Lab/12-04-2021/weather/weather.js
@@ -1,28 +1,34 @@
-function fetchWeather(){
+function fetchWeather()
+{
     var table=document.createElement("TABLE");
     table.border="2";
-    var request=new XMLHttpRequest();
     var apiKey='2578a5dfba20e6f114d57a2840d9561f';
     var city=document.getElementById("city").value;
-    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
-    // console.log("running");
-    request.open('GET',url,true);
-    request.onload=function()
-    {
-        var result=JSON.parse(this.response);
-        result.list.forEach((day)=>{
-            row=table.insertRow(-1);
-            c1=row.insertCell(-1);
-            c2=row.insertCell(-1);
-            c3=row.insertCell(-1);
-            // console.log(day.dt_txt);
-            // console.log(day.main.temp);
-            c1.innerHTML=day.dt_txt;
-            c2.innerHTML=day.main.temp_max;
-            c3.innerHTML=day.main.temp_min;
+    var unit='metric';
+    var url=`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=${unit}`;
+    fetch(url)
+        .then(function(response) { return response.json() }) 
+        .then(function(data) {
+            row=table.insertRow(-1)
+            c1=row.insertCell(-1)
+            c2=row.insertCell(-1)
+            c3=row.insertCell(-1)
+            c1.innerHTML="DATE"
+            c2.innerHTML="MIN(in celsius)"
+            c3.innerHTML="MAX(in celsius)"
+            data.list.forEach((day)=>{
+                row1=table.insertRow(-1);
+                cell1=row1.insertCell(-1)
+                cell2=row1.insertCell(-1)
+                cell3=row1.insertCell(-1)
+                cell1.innerHTML=day.dt_txt
+                cell2.innerHTML=day.main.temp_min
+                cell3.innerHTML=day.main.temp_min
+            })
+            var div=document.getElementById("table");
+            div.append(table);
         })
-    }
-    var div=document.getElementById("table");
-    div.append(table);
-    request.send();
+        .catch(function(err) {
+            console.log(err);
+        });
 }
